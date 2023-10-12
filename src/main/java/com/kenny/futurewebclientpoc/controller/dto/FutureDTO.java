c0229f2a36f94af4d80f06d89dbfa0e01a62156b
kenny.k <kenny.k@kakaobank.com>
2023-10-12 21:09:03 +0900

    feat: Controller, Service 개발
    1. CompletableFuture를 리턴하는 Controller, Service 클래스 개발
    2. Controller에서 사용하는 DTO 클래스 개발
    
    issue: REQ-1

---
+++ src/main/java/com/kenny/futurewebclientpoc/controller/dto/FutureDTO.java
@@ -0,0 +1,15 @@
+package com.kenny.futurewebclientpoc.controller.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+public class FutureDTO {
+
+    @Getter
+    @Builder
+    @ToString
+    public static class Out {
+        private final String endDateTime;
+    }
+}
