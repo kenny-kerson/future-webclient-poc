813d8037525ffc4f3c829eb17613d6f658229d30
kenny-kerson <kenny.kerson@gmail.com>
2023-10-12 22:55:37 +0900

    feat: WebClient 셋팅 및 호출로직 구성
    1. WebClientConfiguration 클래스 이름 변경 : WebConfig
    2. WebConfig에 WebClient.Builder Bean 등록
    3. WebClient.Builder를 주입받아서 external mock api를 webClient로 호출하는 로직 개발
    
    issue: REQ-3

--- src/main/java/com/kenny/futurewebclientpoc/service/WebClientService.java
+++ src/main/java/com/kenny/futurewebclientpoc/service/WebClientService.java
@@ -4,14 +4,31 @@ import com.kenny.futurewebclientpoc.controller.dto.FutureDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.concurrent.CompletableFuture;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class WebClientService {
+
+    private final WebClient webClient;
+
+    public WebClientService(final WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl("http://localhost:8080")
+                .build();
+    }
+
     public CompletableFuture<FutureDTO.Out> api(final String startDateTime) {
-        return null;
+        log.warn("# WebClientService external api call!!");
+
+        return webClient.get()
+                .uri("/external/mock_api")
+                .retrieve()
+                .bodyToMono(FutureDTO.Out.class)
+                .toFuture()
+        ;
     }
 }
