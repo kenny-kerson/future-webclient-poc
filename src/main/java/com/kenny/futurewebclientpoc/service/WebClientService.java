0652cd2a42ca7f95e22d04b8ffe224c3d25c594b
kenny-kerson <kenny.kerson@gmail.com>
2023-10-15 19:34:02 +0900

    feat: API호출횟수, API duration 측정을 위한 Counter, Timer 메트릭 API 추가
    issue: N/A

--- src/main/java/com/kenny/futurewebclientpoc/service/WebClientService.java
+++ src/main/java/com/kenny/futurewebclientpoc/service/WebClientService.java
@@ -1,6 +1,9 @@
 package com.kenny.futurewebclientpoc.service;
 
 import com.kenny.futurewebclientpoc.controller.dto.FutureDTO;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,6 +17,10 @@ public class WebClientService {
 
     private final WebClient webClient;
 
+    // metric
+    private final Counter apiCallCounter = Metrics.counter("external.api.calls.total");
+    private final Timer apiCallTimer = Metrics.timer("external.api.calls.duration");
+
     public WebClientService(final WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder
                 .baseUrl("http://localhost:8081")
@@ -23,45 +30,54 @@ public class WebClientService {
     public CompletableFuture<FutureDTO.Out> nonblockingApi(final String startDateTime) {
         log.warn("# WebClientService external nonblockingApi call!!");
 
-        final CompletableFuture<FutureDTO.Out> apiOutput = webClient.get()
-                .uri("/external/mock_api")
-                .retrieve()
-                .bodyToMono(Long.class)
-                .map(output -> FutureDTO.Out.builder()
-                        .second(String.valueOf(output))
-                        .build())
-                .toFuture()
-                .thenApply(result -> {
+        final CompletableFuture<FutureDTO.Out> apiOutput = apiCallTimer.record(() -> {
+            apiCallCounter.increment();
+
+            return webClient.get()
+                    .uri("/external/mock_api")
+                    .retrieve()
+                    .bodyToMono(Long.class)
+                    .map(output -> FutureDTO.Out.builder()
+                            .second(String.valueOf(output))
+                            .build())
+                    .toFuture()
+                    .thenApply(result -> {
                         // Future의 결과를 받아 새로운 결과를 생성한다.
                         log.warn("# result : {}", result);
                         return result;
-                })
-                // 이 시간동안 Future가 완료되지 않으면 익셉션을 발생시킨다.
-                .orTimeout(20L, TimeUnit.SECONDS)
-                .exceptionally(e -> {
+                    })
+                    // 이 시간동안 Future가 완료되지 않으면 익셉션을 발생시킨다.
+                    .orTimeout(20L, TimeUnit.SECONDS)
+                    .exceptionally(e -> {
                         // Future에서 익셉션이 발생했을때 동작할 Callback을 정의한다. 즉, 오류가 발생했을때 동작하는 로직을 작성한다.
                         log.warn("# CompletableFuture error : ", e);
                         return FutureDTO.Out.builder()
                                 .second("error")
                                 .build();
-                })
-        ;
+                    });
+            }
+        );
 
-        log.warn("# api call end & return ");
+        log.warn("# api call end & return");
         return apiOutput;
     }
 
     public FutureDTO.Out blockingApi(final String startDateTime) {
         log.warn("# WebClientService external blockingApi call!!");
 
-        final FutureDTO.Out apiOutput = webClient.get()
-                .uri("/external/mock_api")
-                .retrieve()
-                .bodyToMono(Long.class)
-                .map(output -> FutureDTO.Out.builder()
-                        .second(String.valueOf(output))
-                        .build())
-                .block();
+        final FutureDTO.Out apiOutput = apiCallTimer.record(() -> {
+                apiCallCounter.increment();
+
+                return webClient.get()
+                        .uri("/external/mock_api")
+                        .retrieve()
+                        .bodyToMono(Long.class)
+                        .map(output -> FutureDTO.Out.builder()
+                                .second(String.valueOf(output))
+                                .build())
+                        .block()
+                ;
+        });
 
         log.warn("# api call end & return ");
         return apiOutput;
